9a26c3b923dd70b3a975932701e7fa4e3105ea78
hysbrand <hysbrand@smartweb.augustatech.edu>
2026-02-02 18:09:07 -0500

    Add files via upload

---
+++ InvoiceTotal/InvoiceTotal/frmInvoiceTotal.cs
@@ -0,0 +1,25 @@
+namespace InvoiceTotal
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
